159efaaba41308574a0b916cc7a032e82038ae72
MichalM <michal.j.musial@gmail.com>
2013-03-24 12:58:00 +0100

    added function body

--- unblockClipboardInteraction.js
+++ unblockClipboardInteraction.js
@@ -1,5 +1,10 @@
 function unblockClipboardInteraction() {
+    var snapshot = document.evaluate('//@onpaste',
+        document, null, XPathResult.UNORDERED_NODE_SNAPSHOT_TYPE, null),
+        numItems = snapshot.snapshotLength - 1;
 
+    for (var i = numItems; i >= 0; i--)
+        snapshot.snapshotItem(i).nodeValue = 'return true';
 }
 
 window.addEventListener('DOMContentLoaded', unblockClipboardInteraction, false);
